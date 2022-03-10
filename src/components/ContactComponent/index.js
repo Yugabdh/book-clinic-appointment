fa1438b535f73746d516c3036948a76ae267ab39
Yugabdh <yugabdhppashte@gmail.com>
2022-03-10 13:33:43 +0530

    Fixed maps

--- src/components/ContactComponent/index.js
+++ src/components/ContactComponent/index.js
@@ -23,12 +23,10 @@ const Content = () => {
         <Col md={12} lg={6}>
         <iframe
           title="contact-address"
-          src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d120635.90510041699!2d73.00041529343119!3d19.113268955168888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d19.2116805!2d73.0907!4m5!1s0x3be7c3996fe09f45%3A0x294645a8c395cf56!2sRagul%20Family%20Dental!3m2!1d19.013997099999997!2d73.0122798!5e0!3m2!1sen!2sin!4v1643097298495!5m2!1sen!2sin" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.129893358775!2d73.01009111479856!3d19.013997087123478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3996fe09f45%3A0x294645a8c395cf56!2sRagul%20Family%20Dental!5e0!3m2!1sen!2sin!4v1646128887067!5m2!1sen!2sin" 
           className="google-maps-ifram"
           loading="lazy"
-          allowFullScreen>
-
-        </iframe>
+          allowFullScreen></iframe>
         </Col>
       </Row>
     </Container>
